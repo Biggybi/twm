96668cbe67f38eda1079a9d24727956bd12f2fe3
biggybi <biggybi@protonmail.com>
2022-11-04 10:18:19 +0100

    mockup pages

--- components/Planning/Planning.tsx
+++ components/Planning/Planning.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
 import {Text, View} from 'react-native';
 import {Card} from '../Card/Cards';
 
 export default function Planning() {
-    return (<View>
-    <Card
-      cardInfos=<View><Text>Coucou</Text></View>
-      showActionButtons = {false}
-      />
+  return (
+    <View>
+      <Card
+        cardInfos=<View><Text>Calendar</Text></View>
+        showActionButtons = {false}
+        />
     </View>)
 }
