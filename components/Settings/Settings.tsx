be9c2989a725d5dbc0a1a77067c14c95d7214393
biggybi <biggybi@protonmail.com>
2022-11-04 10:17:04 +0100

    feat(settings): style, logout button

--- components/Settings/Settings.tsx
+++ components/Settings/Settings.tsx
@@ -1,9 +1,53 @@
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+
+import React from 'react';
+import {TouchableOpacity} from 'react-native';
+import {useUserIDUpdate} from '../../contexts/User/userContext';
+import {Colors} from '../../tools/colors';
 
 export default function Settings() {
+  const toggleUser = useUserIDUpdate() as unknown as (s: string) => string;
   return (
-    <View>
-      <Text>"settings"</Text>
+    <View style={styles.wrapper}>
+      <Text style={styles.title}>Settings</Text>
+      <View style={styles.section}>
+        <Text style={styles.subtitle}>Extra</Text>
+        <TouchableOpacity style={styles.buttons} onPress={() => toggleUser('')}>
+          <Text>Logout</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    alignSelf: 'center',
+    width: '80%',
+  },
+  title: {
+    height: 100,
+    fontWeight: 'bold',
+    fontSize: 30,
+    textAlignVertical: 'center',
+    color: Colors.foreground.dark,
+  },
+  subtitle: {
+    height: 40,
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlignVertical: 'center',
+    color: Colors.foreground.dark,
+  },
+  buttons: {
+    height: 50,
+    backgroundColor: Colors.red.light,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 5,
+  },
+  section: {
+    marginVertical: 20,
+  },
+});
